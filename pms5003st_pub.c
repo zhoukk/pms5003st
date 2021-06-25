03ea96a5ba20a10a0a0e6bd22017e5036f1e3c5f
zhoukk <izhoukk@gmail.com>
2021-06-25 16:16:46 +0800

    fix mqtt

--- pms5003st_pub.c
+++ pms5003st_pub.c
@@ -1,408 +1,113 @@
-#ifdef __linux__
-#define HAVE_EPOLL 1
-#endif
-
-#if (defined(__APPLE__) && defined(MAC_OS_X_VERSION_10_6)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
-#define HAVE_KQUEUE 1
-#endif
-
-#ifdef __sun
-#include <sys/feature_tests.h>
-#ifdef _DTRACE_VERSION
-#define HAVE_EVPORT 1
-#endif
-#endif
-
-#define LIBMQTT_IMPLEMENTATION
-#include "libmqtt.h"
-
-#define LIBEVENT_IMPLEMENTATION
-#include "libevent.h"
+#define MQTT_CLI_LINUX_PLATFORM
+#define MQTT_CLI_IMPL
+#include "mqtt_cli.h"
 
 #define PMS5003ST_IMPLEMENTATION
 #include "pms5003st.h"
 
+static void _connack(mqtt_cli_t *m, void *ud, const mqtt_packet_t *pkt) {
+  (void)m;
+  (void)ud;
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/un.h>
-#include <sys/time.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <netdb.h>
-
-struct pms5003st_mqtt {
-    int ttyfd;
-    int iofd;
-    int timer_id;
-    char devpath[1024];
-    char host[1024];
-    int port;
-    struct libevent *evt;
-    struct libmqtt *mqtt;
-    int connected;
-};
-
-static struct pms5003st_mqtt *P;
-
-static int __pms_connect();
-static int __pms_retry();
-
-static void
-__close(void) {
-    if (-1 != P->iofd) {
-        libevent__del_file(P->evt, P->iofd, LIBEVENT_READABLE);
-        close(P->iofd);
+  if (pkt->ver == MQTT_VERSION_3) {
+    if (pkt->v.connack.v3.return_code != MQTT_CRC_ACCEPTED) {
+      printf("Connack, %s\n", mqtt_crc_name(pkt->v.connack.v3.return_code));
+      return;
     }
-    if (-1 != P->timer_id)
-        libevent__del_time(P->evt, P->timer_id);
+  } else if (pkt->ver == MQTT_VERSION_4) {
+    if (pkt->v.connack.v4.return_code != MQTT_CRC_ACCEPTED) {
+      printf("Connack, %s\n", mqtt_crc_name(pkt->v.connack.v4.return_code));
+      return;
+    }
+  }
 }
 
-static void
-__disconnect(void) {
-    fprintf(stdout, "disconnected: %s\n", strerror(errno));
-    __close();
-    if (__pms_connect()) {
-        if (-1 == libevent__set_time(P->evt, 1000, 0, __pms_retry)) {
-            fprintf(stdout, "libevent__set_time: e: %d %s\n", errno, strerror(errno));
-            return;
-        }
-    }
-}
+int main(int argc, char *argv[]) {
+  mqtt_cli_t *m;
+  void *net;
+  int uart_fd;
 
+  if (argc < 2) {
+    printf("usage: %s host dev\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-static void
-__read(struct libevent *evt, void *ud, int fd, int mask) {
-    int nread;
-    char buff[4096];
-    int rc;
-    (void)evt;
-    (void)ud;
-    (void)mask;
+  mqtt_cli_conf_t config = {
+      .client_id = "pms5003st_pub",
+      .version = MQTT_VERSION_4,
+      .keep_alive = 60,
+      .clean_session = 1,
+      .auth =
+          {
+              .username = "pms5003st_pub",
+              .password = "pub",
+          },
+      .lwt =
+          {
+              .retain = 1,
+              .topic = "pms5003st_pub_state",
+              .qos = MQTT_QOS_1,
+              .message = {.s = "exit", .n = 4},
+          },
+      .cb =
+          {
+              .connack = _connack,
+          },
+      .ud = 0,
+  };
 
-    nread = read(fd, buff, sizeof(buff));
-    if (nread == -1 && errno == EAGAIN) {
-        return;
-    }
-    rc = LIBMQTT_SUCCESS;
-    if (nread <= 0 || LIBMQTT_SUCCESS != (rc = libmqtt__parse(P->mqtt, buff, nread))) {
-        if (rc != LIBMQTT_SUCCESS)
-            fprintf(stderr, "libmqtt__parse: %s\n", libmqtt__strerror(rc));
-        __disconnect();
-    }
-}
+  net = linux_tcp_connect(argv[1], MQTT_TCP_PORT);
+  if (!net) {
+    fprintf(stderr, "mqtt broker connect error\n");
+    return EXIT_FAILURE;
+  }
 
-static int
-__write(void *p, const char *data, int size) {
-    (void)p;
-    return size == write(P->iofd, data, size) ? 0 : -1;
-}
+  uart_fd = uart_open(argv[2]);
+  if (uart_fd < 0) {
+    fprintf(stderr, "fatal: uart_open(): %s: %s\n", argv[2], strerror(errno));
+    return EXIT_FAILURE;
+  }
+  if (0 != uart_set(uart_fd, 9600, 0, 8, 'N', 1)) {
+    fprintf(stderr, "fatal: uart_set(): %s: %s\n", argv[2], strerror(errno));
+    return EXIT_FAILURE;
+  }
 
-static int
-__update(struct libevent *evt, void *ud, int id) {
-    int rc;
-    (void)evt;
-    (void)ud;
-    (void)id;
+  m = mqtt_cli_create(&config);
+  mqtt_cli_connect(m);
 
-    if (LIBMQTT_SUCCESS != (rc = libmqtt__update(P->mqtt))) {
-        fprintf(stdout, "libmqtt__update: %s\n", libmqtt__strerror(rc));
-        return -1;
-    }
-    return 1000;
-}
-
-static int
-__set_non_block(int fd, int non_block) {
-    int flags;
-
-    if ((flags = fcntl(fd, F_GETFL)) == -1) {
-        return -1;
-    }
-
-    if (non_block)
-        flags |= O_NONBLOCK;
-    else
-        flags &= ~O_NONBLOCK;
-
-    if (fcntl(fd, F_SETFL, flags) == -1) {
-        return -1;
-    }
-    return 0;
-}
-
-static int
-__set_tcp_nodelay(int fd, int nodelay) {
-    if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &nodelay, sizeof(nodelay)) == -1) {
-        return -1;
-    }
-    return 0;
-}
-
-static int
-__set_tcp_keepalive(int fd, int keepalive) {
-    if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &keepalive, sizeof(keepalive)) == -1) {
-        return -1;
-    }
-    return 0;
-}
-
-static int
-__tcp_connect(const char *addr, int port) {
-    int fd, rc;
-    char portstr[6];
-    struct addrinfo hints, *servinfo, *p;
-
-    fd = -1;
-    snprintf(portstr, sizeof(portstr), "%d", port);
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((rc = getaddrinfo(addr, portstr, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rc));
-        return -1;
-    }
-    for (p = servinfo; p; p = p->ai_next) {
-        int reuse = 1;
-
-        if ((fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
-            continue;
-        if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1) {
-            fprintf(stderr, "setsockopt: %s\n", strerror(errno));
-            goto err;
-        }
-        if (connect(fd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(fd);
-            fd = -1;
-            continue;
-        }
-        goto end;
-    }
-
-err:
-    if (fd != -1) {
-        close(fd);
-        fd = -1;
-    }
-
-end:
-    freeaddrinfo(servinfo);
-    return fd;
-}
-
-static int
-__connect(void) {
-    int fd;
-    int timer_id;
-
-    fd = __tcp_connect(P->host, P->port);
-    if (-1 == fd) {
-        fprintf(stderr, "__tcp_connect: %s\n", strerror(errno));
-        goto e1;
-    }
-    __set_non_block(fd, 1);
-    __set_tcp_nodelay(fd, 1);
-    __set_tcp_keepalive(fd, 1);
-
-    if (-1 == libevent__set_file(P->evt, fd, LIBEVENT_READABLE, 0, __read)) {
-        fprintf(stderr, "libevent__set_file: %s\n", strerror(errno));
-        goto e2;
-    }
-
-    timer_id = libevent__set_time(P->evt, 1000, 0, __update);
-    if (-1 == timer_id) {
-        fprintf(stderr, "libevent__set_time: %s\n", strerror(errno));
-        goto e3;
-    }
-
-    P->iofd = fd;
-    P->timer_id = timer_id;
-    return 0;
-
-e3:
-    libevent__del_file(P->evt, fd, LIBEVENT_READABLE);
-e2:
-    close(fd);
-e1:
-    return -1;
-}
-
-static int
-__pms_update(struct libevent *evt, void *ud, int id) {
-    int rc;
+  while (1) {
+    mqtt_str_t outgoing, incoming;
+    uint64_t t1, t2;
     struct pms5003st p;
+    mqtt_str_t message;
     char str[1024] = {0};
-    int len;
-    (void)evt;
-    (void)ud;
-    (void)id;
 
-    if (0 == P->connected) {
-        return 100;
+    t1 = linux_time_now();
+    mqtt_cli_outgoing(m, &outgoing);
+    if (linux_tcp_transfer(net, &outgoing, &incoming)) {
+      break;
     }
-    if (0 != pms5003st_read(P->ttyfd, &p)) {
-        return 100;
+    if (mqtt_cli_incoming(m, &incoming)) {
+      break;
     }
-    len = pms5003st_json(&p, str, 1024);
-    rc = libmqtt__publish(P->mqtt, 0, 0, 0, "pms5003st", str, len);
-    if (rc != LIBMQTT_SUCCESS) {
-        fprintf(stdout, "%s\n", libmqtt__strerror(rc));
+    t2 = linux_time_now();
+    if (mqtt_cli_elapsed(m, t2 - t1)) {
+      break;
     }
 
-    len = sprintf(str, "pms5003st,sensor=sensor1,ver=%d,err=%d pm1_0_atm=%d,pm2_5_atm=%d,pm10_atm=%d,"
-            "pm1_0_std=%d,pm2_5_std=%d,pm10_std=%d,g_0_3um=%d,g_0_5um=%d,g_1_0um=%d,g_2_5um=%d,g_5_0um=%d,g_10um=%d,"
-            "hcho=%.3f,temperature=%.1f,humidity=%.1f", p.ver, p.err, p.pm1_0_atm, p.pm2_5_atm, p.pm10_atm,
-            p.pm1_0_std, p.pm2_5_std, p.pm10_std, p.g_0_3um, p.g_0_5um, p.g_1_0um, p.g_2_5um, p.g_5_0um, p.g_10um,
-            p.hcho, p.temperature, p.humidity);
-    rc = libmqtt__publish(P->mqtt, 0, 0, 0, "pms5003st_influxdb", str, len);
-    if (rc != LIBMQTT_SUCCESS) {
-        fprintf(stdout, "%s\n", libmqtt__strerror(rc));
+    if (0 != pms5003st_read(uart_fd, &p)) {
+      continue;
     }
-
-    len = sprintf(str, "%.1f", p.temperature);
-    rc = libmqtt__publish(P->mqtt, 0, 0, 0, "pms5003st_temperature", str, len);
-    if (rc != LIBMQTT_SUCCESS) {
-        fprintf(stdout, "%s\n", libmqtt__strerror(rc));
+    pms5003st_print(&p);
+    message.n = pms5003st_json(&p, str, 1024);
+    message.s = str;
+    if (mqtt_cli_publish(m, 0, "pms5003st", MQTT_QOS_0, &message, 0)) {
+      break;
     }
+  }
 
-    len = sprintf(str, "%.1f", p.humidity);
-    rc = libmqtt__publish(P->mqtt, 0, 0, 0, "pms5003st_humidity", str, len);
-    if (rc != LIBMQTT_SUCCESS) {
-        fprintf(stdout, "%s\n", libmqtt__strerror(rc));
-    }
-
-    len = sprintf(str, "%d", p.pm2_5_std);
-    rc = libmqtt__publish(P->mqtt, 0, 0, 0, "pms5003st_pm2_5", str, len);
-    if (rc != LIBMQTT_SUCCESS) {
-        fprintf(stdout, "%s\n", libmqtt__strerror(rc));
-    }
-
-    return 100;
-}
-
-static int
-__pms_connect(void) {
-    int rc;
-
-    rc = __connect();
-    if (rc) {
-        fprintf(stdout, "__connect: %s\n", strerror(errno));
-        return -1;
-    }
-    rc = libmqtt__connect(P->mqtt, 0, __write);
-    if (rc != LIBMQTT_SUCCESS) {
-        fprintf(stdout, "%s\n", libmqtt__strerror(rc));
-        __close();
-    }
-    return 0;
-}
-
-static int
-__pms_retry(struct libevent *evt, void *ud, int id) {
-    (void)evt;
-    (void)ud;
-    (void)id;
-
-    if (__pms_connect()) {
-        return 1000;
-    }
-    return -1;
-}
-
-static void
-__connack(struct libmqtt *mqtt, void *ud, int ack_flags, enum mqtt_crc return_code) {
-    (void)ack_flags;
-    (void)ud;
-
-    if (return_code != MQTT_CRC_ACCEPTED) {
-        fprintf(stdout, "%s\n", MQTT_CRC_NAMES[return_code]);
-        return;
-    }
-
-    if (-1 == P->ttyfd) {
-        P->ttyfd = uart_open(P->devpath);
-        if (P->ttyfd < 0) {
-            fprintf(stdout, "fatal: open(): %s: %s\n", P->devpath, strerror(errno));
-            libmqtt__disconnect(mqtt);
-            return;
-        }
-        uart_set(P->ttyfd, 9600, 0, 8, 'N', 1);
-        P->connected = 1;
-        if (-1 == libevent__set_time(P->evt, 100, 0, __pms_update)) {
-            fprintf(stdout, "libevent__set_time: error\n");
-            libmqtt__disconnect(mqtt);
-            return;
-        }
-    }
-}
-
-static void
-__puback(struct libmqtt *mqtt, void *ud, uint16_t id) {
-    (void)mqtt;
-    (void)ud;
-    (void)id;
-}
-
-int
-main(int argc, char *argv[]) {
-    int rc;
-    struct libmqtt *mqtt;
-    struct libmqtt_cb cb = {
-        .connack = __connack,
-        .puback = __puback,
-    };
-    struct pms5003st_mqtt pm;
-
-    if (argc < 3) {
-        printf("usage: %s devpath host port\n", argv[0]);
-        return 0;
-    }
-
-    memset(&pm, 0, sizeof pm);
-    strcpy(pm.devpath, argv[1]);
-    strcpy(pm.host, argv[2]);
-
-    if (argc > 3) {
-        pm.port = atoi(argv[3]);
-    } else {
-        pm.port = 1883;
-    }
-    pm.evt = libevent__create(128);
-    pm.ttyfd = -1;
-
-    P = &pm;
-
-    rc = __connect();
-    if (rc) {
-        return 0;
-    }
-
-    rc = libmqtt__create(&mqtt, "pms5003st_pub", &pm, &cb);
-    if (!rc) libmqtt__will(mqtt, 1, 2, "pms5003st_pub_state", "exit", 4);
-    if (rc != LIBMQTT_SUCCESS) {
-        fprintf(stdout, "%s\n", libmqtt__strerror(rc));
-        return 0;
-    }
-    pm.mqtt = mqtt;
-
-    if (__pms_connect()) {
-        if (-1 == libevent__set_time(pm.evt, 1000, 0, __pms_retry)) {
-            fprintf(stdout, "libevent__set_time: e: %d %s\n", errno, strerror(errno));
-            return 0;
-        }
-    }
-
-    libevent__loop(pm.evt);
-    libevent__destroy(pm.evt);
-    libmqtt__destroy(mqtt);
-    if (pm.ttyfd != -1) {
-        uart_close(pm.ttyfd);
-    }
-    return 0;
+  mqtt_cli_destroy(m);
+  linux_tcp_close(net);
+  uart_close(uart_fd);
+  return 0;
 }
